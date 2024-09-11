80f88d7fa5e36687824a3448f38e1c17a59f5f7c
Lucas Aster <75588337+Asterlc@users.noreply.github.com>
2024-09-11 19:16:00 -0300

    Update main.js
    adiciona headers no resolvee seta encoding default para utf8

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const http = require('http');
 const fetch = (uri, options) => {
     return new Promise((resolve, reject) => {
         const req = http.request(uri, options, (res) => {
+            res.setEncoding('utf8');
             let data = '';
             res.on('data', (chunk) => {
                 data += chunk;
@@ -10,6 +11,7 @@ const fetch = (uri, options) => {
             res.on('end', () => {
                 resolve({
                     status: res.statusCode,
+                    headers: res.headers,
                     data: data
                 });
             });
